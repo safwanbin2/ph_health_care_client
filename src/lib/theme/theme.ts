ca96591355437503ea49ee059c22d49c100d006c
safwanbin2 <safwanridwan321@gmail.com>
2024-10-11 22:36:04 +0600

    [edit] theme

--- src/lib/theme/theme.ts
+++ src/lib/theme/theme.ts
@@ -3,7 +3,8 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#73EC8B",
+      // main: "#73EC8B",
+      main: "#0D92F4",
     },
     secondary: {
       main: "#D8A25E",
