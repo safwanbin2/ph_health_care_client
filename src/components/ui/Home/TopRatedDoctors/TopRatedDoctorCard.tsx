414390633a816a8010ddac2dce2cb2e5e7c32e6d
safwanbin2 <safwanridwan321@gmail.com>
2024-10-11 22:34:27 +0600

    [edit] home>topDoctorCard

--- src/components/ui/Home/TopRatedDoctors/TopRatedDoctorCard.tsx
+++ src/components/ui/Home/TopRatedDoctors/TopRatedDoctorCard.tsx
@@ -1,15 +1,22 @@
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { TDoctor } from "@/interfaces";
 import { Grid, Stack } from "@mui/material";
 import Image from "next/image";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const TopRatedDoctorCard = ({ doctor }: { doctor: TDoctor }) => {
-  const { profilePhoto, name } = doctor;
+  const {
+    profilePhoto,
+    name,
+    designation,
+    address,
+    qualification,
+    currentWorkingPlace,
+  } = doctor;
   return (
     <Grid item md={4}>
       <Card>
@@ -23,12 +30,18 @@ const TopRatedDoctorCard = ({ doctor }: { doctor: TDoctor }) => {
           <Typography gutterBottom variant="h5" component="div">
             {name}
           </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Typography color="gray">
+            {qualification}, {currentWorkingPlace}
+          </Typography>
+          <Typography color="gray" mt={1} fontSize={14}>
+            <LocationOnIcon /> {address}
           </Typography>
         </CardContent>
-        <CardActions>
+        <CardActions
+          sx={{
+            p: 2,
+          }}
+        >
           <Stack direction="row" gap={1}>
             <Button>Book Now</Button>
             <Button variant="outlined">View Profile</Button>
