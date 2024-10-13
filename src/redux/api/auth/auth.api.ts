9ab6d77a6dfccdb578d56073748a4c894fbfe24c
safwanbin2 <safwanridwan321@gmail.com>
2024-10-13 22:15:37 +0600

    [add] axios config with redux

--- src/redux/api/auth/auth.api.ts
+++
@@ -1,7 +0,0 @@
-import { baseApi } from "../baseApi";
-
-const authApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({}),
-});
-
-export const {} = authApi;
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,9 +1,10 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_BACKEND_API_URL,
+  baseQuery: axiosBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_BACKEND_API_URL as string,
   }),
-  endpoints: (builder) => ({}),
+  endpoints: () => ({}),
 });
