880d4ac518e52f4b717f90b2d28e3b51700e45ad
safwanbin2 <safwanridwan321@gmail.com>
2024-10-15 19:47:43 +0600

    [add] basic routes for patients

---
+++ src/app/(withDashboardLayout)/dashboard/patient/payment-history/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PaymentHistoryPage = () => {
+  return (
+    <div>
+      <h2>Payment history page</h2>
+    </div>
+  );
+};
+
+export default PaymentHistoryPage;
---
+++ src/app/(withDashboardLayout)/dashboard/patient/prescriptions/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrescriptionsPage = () => {
+  return (
+    <div>
+      <h2>Prescriptions page</h2>
+    </div>
+  );
+};
+
+export default PrescriptionsPage;
